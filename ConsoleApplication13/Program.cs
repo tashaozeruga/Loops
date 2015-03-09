e5f5b2519e5bcd232b9045d65d9cfe808f3c3e6b
tashaozeruga <tashaozeruga@gmail.com>
2015-03-09 22:23:42 +0200

    Some code with loop

---
+++ ConsoleApplication13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] d = { 2, 6, 7, 9, 3 };
+            foreach (int s in d)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            
+
+
+
+           
+
+           
+        }
+    }
+}
